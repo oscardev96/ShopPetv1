2fd71727b37e96b6c414ebd8bdd19db13aded604
Can Dat <candatbg@gmail.com>
2021-04-06 04:43:06 +0700

    add file

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-
+import {FONTS, COLORS} from './src/constants/theme';
 const App = () => {
   return (
-    <View>
-      <Text>jojo</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{...FONTS.h2, color: COLORS.primary}}>Shop Pet</Text>
     </View>
   );
 };
